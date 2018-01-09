18fe6bd20cbe0cd7964fcc66fd8731b1405a4f54
Yuta Koga <ykoga.kyutech@gmail.com>
2018-01-09 09:06:39 +0000

    Fix method name

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,7 +12,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get contact_path
-    assert_select "title", full_path("Contact")
+    assert_select "title", full_title("Contact")
 
   end
 
