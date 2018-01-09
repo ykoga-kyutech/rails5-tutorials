7d2de589d00e88398bd5f50849da59c029c05d5c
Yuta Koga <ykoga.kyutech@gmail.com>
2018-01-09 09:02:07 +0000

    Enable to use applicatin helper on the test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -11,6 +11,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get contact_path
+    assert_select "title", full_path("Contact")
+
   end
 
 end
