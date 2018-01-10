77df7610f34958c16b39e41ca603aeba4793ed55
Yuta Koga <ykoga.kyutech@gmail.com>
2018-01-10 08:32:57 +0000

    Add regular exp. check for email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -21,4 +21,23 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "    "
     assert_not @user.valid?
   end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be valid"
+    end
+  end
 end
